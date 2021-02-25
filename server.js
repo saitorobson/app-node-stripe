14658ef9c1965f17a18728d5b06a71127e700a9c
Robson Saito <robsonsaito@Robsons-MacBook-Air.local>
2021-02-25 00:59:27 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const stripe = require('stripe')('STRIPE_SECRET_KEY');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static(path.join(__dirname, './views')));
+
+app.post('/charge', (req, res) => {
+    try {
+        stripe.customers
+            .create({
+                name: req.body.name,
+                email: req.body.email,
+                source: req.body.stripeToken
+            })
+            .then(customer => 
+                stripe.charges.create({
+                    amount: req.body.amount * 100,
+                    currency: 'brl',
+                    customer: customer.id
+                })
+            )
+            .then(() => res.render('completed.html'))
+            .catch(err => console.log(err));
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Server is running...'));
---
+++ views/card.js
@@ -0,0 +1,33 @@
+const stripe = Stripe('STRIPE_PUBLISHABLE_KEY');
+const elements = stripe.elements();
+
+var style = {
+    base: {
+        color: '#fff'
+    }
+};
+
+const card = elements.create('card', { style });
+card.mount('#card-element');
+
+const form = document.querySelector('form');
+const errorEl = document.querySelector('#card-errors');
+
+const stripeTokenHandler = token => {
+    const hiddenInput = document.createElement('input');
+    hiddenInput.setAttribute('type', 'hidden');
+    hiddenInput.setAttribute('name', 'stripeToken');
+    hiddenInput.setAttribute('value', token.id);
+    form.appendChild(hiddenInput);
+
+    form.submit();
+}
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    stripe.createToken(card).then(res => {
+        if (res.error) errorEl.textContent = res.error.message;
+        else stripeTokenHandler(res.token);
+    })
+})
